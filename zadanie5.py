73b07729777963c7e8d7d4b299131c126582d7b3
rustamiwanttobefree <76264260+rustamiwanttobefree@users.noreply.github.com>
2020-12-20 23:10:32 +0300

    Add files via upload

---
+++ zadanie5.py
@@ -0,0 +1,9 @@
+print("Издержки:")
+x = int(input())
+print("выручка:")
+y = int(input())
+if x < y:
+    print("Прибыль")
+print(" убыток")
+
+
---
+++ zadanie6.py
@@ -0,0 +1,12 @@
+print("Введите x:")
+x = int(input())
+print("Введите y:")
+y = int(input())
+s = x
+k = 1
+while s <= y:
+    k = k+1
+    s = 1.1*s
+print("k = ", k)
+
+
